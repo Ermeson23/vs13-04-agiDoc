d59ed892477fa395ed331fd4c2aa49f5ba1b2dba
vivalaclara <claramaestri07@gmail.com>
2024-01-22 23:57:03 -0300

    fixed navbar styles

--- src/components/menu/Menu.tsx
+++ src/components/menu/Menu.tsx
@@ -1,42 +1,28 @@
-import React, { useState } from 'react';
-import Box from '@mui/material/Box';
+
 import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
 import Toolbar from '@mui/material/Toolbar';
 import IconButton from '@mui/material/IconButton';
 import MenuIcon from '@mui/icons-material/Menu';
 import logo from '../../assets/agidoclogo.png';
-import NavBar from '../navBar/NavBar';
-
+import './menu.css';
 
 export default function ButtonAppBar() {
-  const [navBarVisible, setNavBarVisible] = useState(false);
-
-  const handleMenuClick = () => {
-    setNavBarVisible(!navBarVisible);
-  };
-
   return (
     <Box sx={{ flexGrow: 1 }}>
       <AppBar position="static" className='custom-appbar'>
         <Toolbar className='custom-toolbar'>
-          <img src={logo} alt="Logo" style={{ height: '2.5rem', marginRight: '0.625rem' }} />
-          <IconButton
-            className='custom-iconbutton'
+        <img src={logo} alt="Logo" style={{ height: '2.5rem', marginRight: '0.625rem' }} />
+          <IconButton className='custom-iconbutton'
             size="large"
             edge="start"
             aria-label="menu"
             sx={{ mr: 2 }}
-            onClick={handleMenuClick}
           >
             <MenuIcon />
           </IconButton>
-          {navBarVisible && <NavBar />}
         </Toolbar>
       </AppBar>
     </Box>
   );
 }
-
-
-
-
