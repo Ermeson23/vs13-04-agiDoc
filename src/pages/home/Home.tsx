b577900b9832b8f7eb2a726f0bf20f2bd6ef7e54
vivalaclara <claramaestri07@gmail.com>
2024-01-23 23:25:43 -0300

    about section styles and responsivity

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,5 +1,6 @@
 import Header from "../../components/header/Header"
 import IntroSection from "../../components/homeComponents/introSection"
+import AboutSection from "../../components/homeComponents/aboutSection"
 import '../../App.css'
 import './home.css'
 
@@ -11,9 +12,12 @@ export default function Home(){
         <Header></Header>
         </section>
         <section className="main-section">
-        <div className="intro-container">
+        <div className="intro-container container">
           <IntroSection></IntroSection>
 
+        </div>
+        <div className="about-container container">
+          <AboutSection></AboutSection>
         </div>
         </section>
         
