1bc5a0e36aa82338cd132c08361492c6f134ede5
unknown <Ermeson23>
2024-01-25 03:59:27 -0300

    Fix: Apply logic in register page

--- src/pages/register/Register.tsx
+++ src/pages/register/Register.tsx
@@ -6,67 +6,102 @@ import '../../index.css';
 import './Register.css';
 
 interface UserRegistrationResponse {
-  id: number;
-  error?: string;
+    id: number;
+    error?: string;
 }
 
 interface Register {
-  name: string;
-  cpf: string,
-  email: string
-  password: string;
-  passwordConfirmation: string
+    id: string,
+    name: string;
+    cpf: string,
+    email: string
+    password: string;
+    passwordConfirmation: string
 }
 
 const Register = () => {
-  const { register, handleSubmit, formState: { errors }, reset } = useForm<Register>();
-  
-  const onSubmit: SubmitHandler<Register> = async (data) => {
-    try {
-      const response: AxiosResponse<UserRegistrationResponse> = await axios.post('http://localhost:3001/users', data);
-      alert(`Usuário cadastrado com o ID:', ${response.data.id}`);
-      const navigate = useNavigate();
-      navigate('/login');
-      reset();
-    } catch (error) {
-      alert('Usuário não cadastrado');
-    }
-  };
+    const { register, handleSubmit, formState: { errors }, reset } = useForm<Register>();
+    const navigate = useNavigate();
 
-  return (
-    <div className="register-container">
-      <h1>Cadastrar usuário</h1>
-      <form onSubmit={handleSubmit(onSubmit)}>
-        <h2 className='title'>Cadastro</h2>
-        <div className="form-group">
-          <label htmlFor="name">Nome:</label>
-          <input id="name" type="text" {...register('name', { required: true })} />
-          {errors.name && <span>Este campo é obrigatório</span>}
+    const onSubmit: SubmitHandler<Register> = async (data) => {
+
+        const { passwordConfirmation, ...dataToSend } = data;
+
+        try {
+
+            if (dataToSend.password !== passwordConfirmation) {
+                alert('A senha e a confirmação de senha não coincidem');
+                return;
+            }
+
+            const emailExists = await axios.get('http://localhost:3001/users', {
+                params: { email: dataToSend.email }
+            });
+
+            if (emailExists.data.length > 0) {
+                alert('Email já cadastrado!');
+                return;
+            }
+
+            const response: AxiosResponse<UserRegistrationResponse> = await axios.post('http://localhost:3001/users', dataToSend);
+            alert(`Usuário cadastrado com o ID:', ${response.data.id}`);
+            navigate('/login');
+            reset();
+        } catch (error) {
+            alert('Usuário não cadastrado');
+        }
+    };
+
+    return (
+        <div className="register-container">
+            <h1>Cadastrar usuário</h1>
+            <form onSubmit={handleSubmit(onSubmit)}>
+                <h2 className='title'>Cadastro</h2>
+                <div className="form-group">
+                    <label htmlFor="name">Nome:</label>
+                    <input id="name" type="text" {...register('name', { required: true })} />
+                    {errors.name && <span>Este campo é obrigatório</span>}
+                </div>
+                <div className="form-group">
+                    <label htmlFor="cpf">CPF:</label>
+                    <input id="cpf" type="text" {...register('cpf', {
+                        required: true, pattern: {
+                            value: /^[0-9]{11}$/,
+                            message: "O CPF deve ter exatamente 11 dígitos."
+                        }
+                    })} />
+                    {errors.cpf && <span>{errors.cpf.message}</span>}
+                </div>
+                <div className="form-group">
+                    <label htmlFor="email">Seu e-mail:</label>
+                    <input id="email" type="email" {...register('email', { required: true })} />
+                    {errors.email && <span>Este campo é obrigatório</span>}
+                </div>
+                <div className="form-group">
+                    <label>Senha:</label>
+                    <input type="password" {...register('password', {
+                        required: true, minLength: {
+                            value: 6,
+                            message: "A senha deve ter pelo menos 6 caracteres."
+                        },
+                        maxLength: {
+                            value: 15,
+                            message: "A senha não pode ter mais de 15 caracteres."
+                        }
+                    })} />
+                    {errors.password && <span>{errors.password.message}</span>}
+                </div>
+                <div className="form-group">
+                    <label>Confirmar a Senha:</label>
+                    <input type="password" {...register('passwordConfirmation', {
+                        required: true
+                    })} />
+                    {errors.passwordConfirmation && <span>Este campo é obrigatório.</span>}
+                </div>
+                <button type="submit">Criar</button>
+            </form>
         </div>
-        <div className="form-group">
-          <label htmlFor="cpf">CPF:</label>
-          <input id="cpf" type="text" {...register('cpf', { required: true })} />
-          {errors.cpf && <span>Este campo é obrigatório</span>}
-        </div>
-        <div className="form-group">
-          <label htmlFor="email">Seu e-mail:</label>
-          <input id="email" type="text" {...register('email', { required: true })} />
-          {errors.email && <span>Este campo é obrigatório</span>}
-        </div>
-        <div className="form-group">
-          <label>Senha:</label>
-          <input type="password" {...register('password', { required: true })} />
-          {errors.password && <span>Este campo é obrigatório</span>}
-        </div>
-        <div className="form-group">
-          <label>Confirmar a Senha:</label>
-          <input type="password" {...register('passwordConfirmation', { required: true })} />
-          {errors.passwordConfirmation && <span>Este campo é obrigatório</span>}
-        </div>
-        <button type="submit">Criar</button>
-      </form>
-    </div>
-  );
+    );
 };
 
 export default Register;
